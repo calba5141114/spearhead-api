9f189cc1bbd0f3ee21d9845c6a72de7c846d2684
Sean <Spglancy@gmail.com>
2018-08-21 13:10:11 -0700

    reformatted user schema

--- models/user.js
+++ models/user.js
@@ -9,36 +9,28 @@ const userSchema = mongoose.schema ({
     email:{
         type: String,
         required: [true, "The Email field is required"],
-        
+        unique: true
     },
     bio:{
         type: String,
         required: [true, "The Bio field is required"],
     },
-    track:{
-        type: String,
-    },
-    interests:{
-        type: String,
-    },
-    portfolio:{
-        type: String,
-    },
-    phone:{
-        type: String,
-    },
-    social:{
-        type:String,
-    },
-    room:{
-        type: String,
-    },
-    hometown:{
-        type: String,
-    },
-    photo:{
-        type: ImageData
-    }
+
+    track: String,
+
+    interests: String,
+
+    portfolio: String,
+
+    phone: String,
+
+    social: String,
+
+    room: String,
+
+    hometown: String,
+
+    photo: Image
 
 
 });
